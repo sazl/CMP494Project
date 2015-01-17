9d83074677e828a407de00a320e79a5f8750059c
Sami Zeinelabdin <samiabdin@gmail.com>
2015-01-17 18:56:58 +0400

    Remove unneeded bin files

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd bin && rm -rf mandelbrot mandelbrot_parallel Makefile obj && \
+cd bin && rm -rf mandelbrot mandelbrot_parallel mandelbrot_gui Makefile obj moc_* && \
 cd ../src && make clean && \
 cd ../output && rm -f *.png
